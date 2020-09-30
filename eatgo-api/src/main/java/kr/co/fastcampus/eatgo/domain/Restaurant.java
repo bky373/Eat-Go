3b2f0338ce18901f32f6263afa0a60e48d499aec
bky373 <bo373@naver.com>
2020-09-30 16:04:00 +0900

    feat(domain) : add MenuItem and MenuItemRepository

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -1,10 +1,14 @@
 package kr.co.fastcampus.eatgo.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Restaurant {
 
     private final Long id;
     private final String name;
     private final String address;
+    private final List<MenuItem> menuItems = new ArrayList<>();
 
     public Restaurant(Long id, String name, String address) {
         this.id = id;
@@ -12,7 +16,9 @@ public class Restaurant {
         this.address = address;
     }
 
-    public Long getId() { return id; }
+    public Long getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
@@ -24,5 +30,19 @@ public class Restaurant {
 
     public String getInformation() {
         return name + " in " + address;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        for (MenuItem menuItem : menuItems) {
+            addMenuItem(menuItem);
+        }
     }
 }
