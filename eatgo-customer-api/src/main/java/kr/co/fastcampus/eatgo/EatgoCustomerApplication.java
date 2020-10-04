d6bf7a9d7111a605c16b721879939c5ef4982f54
bky373 <bo373@naver.com>
2020-10-04 19:52:52 +0900

    feat(resources) : add application.yml

--- eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/EatgoCustomerApplication.java
+++ eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/EatgoCustomerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EatgoCustomerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EatgoCustomerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EatgoCustomerApplication.class, args);
+    }
 
 }
