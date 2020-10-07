ac214ded4aa86ca3095548735ac660ec15f79c84
bky373 <bo373@naver.com>
2020-10-07 15:12:17 +0900

    style(api) : add api to middle of each application name

--- eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/EatgoCustomerApiApplication.java
+++ eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/EatgoCustomerApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EatgoCustomerApplication {
+public class EatgoCustomerApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EatgoCustomerApplication.class, args);
+        SpringApplication.run(EatgoCustomerApiApplication.class, args);
     }
 
 }
